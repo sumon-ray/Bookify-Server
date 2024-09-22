e1e9965580fd06a41a1f49649fccf6a3cd59c8cd
Mdafsarx <mdafsar99009@gmail.com>
2024-09-22 10:33:13 +0600

    conect with mongodb

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 const express = require('express')
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const app = express();
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 4000;
 
-
+// home path
 app.get("/", (req, res) => {
     res.send('run properly')
 })
 
+// Mongodb
+const uri = process.env.MONGODB_URL
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
+
+// Data base
+async function run() {
+    try {
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+// the port
 app.listen(port, () => {
-    console.log('bookify server is runing properly')
+    console.log('bookify server is run properly')
 })
