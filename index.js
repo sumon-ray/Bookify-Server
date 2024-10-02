8b174b000443a44af0983c3b93c7a57dee672b8e
Mdafsarx <mdafsar99009@gmail.com>
2024-10-02 20:46:39 +0600

    edited books api for filter the email based data

--- index.js
+++ index.js
@@ -33,17 +33,22 @@ async function run() {
         const Bookify = client.db('Bookify');
         // collection
         const books = Bookify.collection('books');
+        const test = Bookify.collection('test');
 
 
         // Get all books or get by genre
         app.get('/books', async (req, res) => {
             const genre = req.query.genre;
             const search = req.query.search;
+            const email = req.query.email;
             let query = {};
 
             if (genre) { query = { genre } }
             else if (search) {
-                query = { title: { $regex: search , $options: "i"} }
+                query = { title: { $regex: search, $options: "i" } }
+            }
+            else if(email){
+                query = {AuthorEmail:email}
             }
 
             const result = await books.find(query).toArray();
@@ -71,8 +76,11 @@ async function run() {
 
 
 
-
-
+        //    test api
+        app.post('/test', async (req, res) => {
+            const result = await test.insertOne(req.body);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
@@ -89,44 +97,3 @@ app.listen(port, () => {
     console.log('bookify server is run properly')
 })
 
-
-
-
-
-
-
-
-const data = [
-    {
-        "book_id": 101,
-        "title": "The Great Gatsby",
-        "author": "F. Scott Fitzgerald",
-        "genre": "Fiction",
-        "year_published": 1925,
-        "condition": "Good"
-    },
-    {
-        "book_id": 102,
-        "title": "To Kill a Mockingbird",
-        "author": "Harper Lee",
-        "genre": "Historical Drama",
-        "year_published": 1960,
-        "condition": "Excellent"
-    },
-    {
-        "book_id": 103,
-        "title": "1984",
-        "author": "George Orwell",
-        "genre": "Dystopian",
-        "year_published": 1949,
-        "condition": "Fair"
-    },
-    {
-        "book_id": 104,
-        "title": "Pride and Prejudice",
-        "author": "Jane Austen",
-        "genre": "Romance",
-        "year_published": 1813,
-        "condition": "New"
-    }
-]
